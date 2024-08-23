bb7d649ecb05b114d829d65a59fcbf3de6a0b80d
GEOVANAAPROGRAMMER <rm99646@fiap.com.br>
2024-08-22 22:08:21 -0300

    Variáveis e seus tipos.

---
+++ JAVA SCRIPT/3_exercicio.js
@@ -0,0 +1,27 @@
+/* JS não é uma linguagem fortemente tipada */
+
+/* Exemplo de variável indefinida */
+var vIndefinida;
+console.log(typeof vIndefinida);
+
+/* Exemplo de variável booleana */
+var vBooleano = true;
+console.log(typeof vBooleano);
+
+/* Exemplo de variável alfanumérica (String) */
+var vAlfaNumerica = "Geovana Ribeiro Silva";
+console.log(typeof vAlfaNumerica);
+
+/* Exemplo de variável numérica */
+var vNumeroInteriro = -40;
+console.log(typeof vNumeroInteriro);
+
+/* Exemplo de variável de números fracionados */
+var vNumeroFracionado = 40.55;
+console.log(typeof vNumeroFracionado);
+
+/* Exemplo de variável array */
+var vArrayNums = [10, 20, 30];
+console.log(typeof vArrayNums);
+
+
